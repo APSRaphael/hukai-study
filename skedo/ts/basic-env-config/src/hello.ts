accf542a856d58bfb2a4fdb69bc2e1b2788de71d
Raphael <nsaraphael@gmail.com>
2022-09-05 09:42:59 +0800

    feat: ts基本配置

---
+++ skedo/ts/basic-env-config/src/hello.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: 益智
+ * @Date: 2022-09-02 16:11:21
+ * @LastEditTime: 2022-09-02 16:34:29
+ * @LastEditors: 益智
+ * @Description:
+ */
+export function log(x: number) {
+	console.log(x);
+}
+
+log(123);
